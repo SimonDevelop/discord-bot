ed1e096f814ebd0b8e8bbac9a23b8d2038038f18
Simon Micheneau <contact@simon-micheneau.fr>
2018-06-19 14:55:53 +0200

    First Release

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 // Initialisation de la librairie
-const api = require("./api.json")
+const config = require("./config.json")
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-
 
 // Initialisation des classes
 const Help = require('./commands/help')
 const Youtube = require('./commands/youtube')
 const Clear = require('./commands/clear')
 
-
 // Code exécuté lors de la connection du bot
 bot.on('ready', () => {
-  bot.user.setGame("Développer").catch(console.error)
-  console.log("Je suis connecté !")
+  console.log("Bot ready !")
 })
-
 
 // Message de bienvenue au nouveaux membre du serveur discord (MP)
 bot.on('guildMemberAdd', (member) => {
   member.createDM().then((channel) => {
-    return channel.send(`Bienvenue `+ member.displayName +` sur le Discord de (Votre serveur)) !\n\n`
+    return channel.send(`Bienvenue `+ member.displayName +` du serveur Discord `+ config.server +` !\n\n`
     +`Si vous voulez connaitre les divers commandes qui vous sont disponible pour communiquer avec moi, tapez la commande "!help".\n`
-    +`Mon développeur est "(Vous)", il espére que je puisse vous être utile :)`)
+    +`J'espère vous être utile :)`)
   }).catch(console.error)
 })
-
 
 // Les divers commands actives
 bot.on('message', (message) => {
@@ -35,6 +30,5 @@ bot.on('message', (message) => {
   Clear.parse(message)
 })
 
-
 // Token du propriétaire du bot
-bot.login(api.key)
+bot.login(config.key)
