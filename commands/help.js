cbf75212a9a9383c7a9a706bbadd79786f2ceaff
Simon Micheneau <contact@simon-micheneau.fr>
2018-06-20 11:43:15 +0200

    First Release

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = class Help extends Command {
   // Si reconnaissance, alors executer l'action
   static action (message) {
 
-    // On répond à l'auteur du message
+    // On répond à l'auteur du message (commandes publiques)
     message.reply(`Voici ma liste de commandes disponibles :\n\n`
       +`- Pour lancer une lecture de vidéo youtube -> "!yt https://www.youtube.com/watch?v=XXXXXXXXXXX"\n`
       +`- Pour stoper la lecture de vidéo youtube -> "!yt stop"`)
